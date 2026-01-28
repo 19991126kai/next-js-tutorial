cc6f9bd2e59918c9dea6cf07536a5615f7d1dd7c
19991126kai <19991126kai@gmail.com>
2026-01-28 14:56:11 +0900

    セクション4:ServerComponents

---
+++ app/SlowComponent.tsx
@@ -0,0 +1,6 @@
+export default async function SlowComponent() {
+  await new Promise((resolve) => {
+    setTimeout(resolve, 3000);
+  });
+  return <div>重いコンポーネント</div>;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
-export default function Home() {
-  return <h1>Hello World!</h1>;
+import { Suspense } from "react";
+import SlowComponent from "./SlowComponent";
+
+export default async function Home() {
+  return (
+    <div>
+      <h1>メインコンテンツ(すぐに表示)</h1>
+      <Suspense fallback={<div>Loading...</div>}>
+        <SlowComponent />
+      </Suspense>
+    </div>
+  );
 }
